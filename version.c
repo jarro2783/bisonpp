8c1c20bc664de21aa64ab02929074b48ec9d534c
tguthrie <tguthrie@aee6e69a-d91e-67fb-6b70-76d5eebb7037>
2011-05-10 10:36:56 +0000

    Changed bison++ version string to 1.21-45 (indicating gcc 4.5 compatible).
    git-svn-id: http://flexpp-bisonpp.googlecode.com/svn/bison++/trunk-bison++-1.21-8@11 aee6e69a-d91e-67fb-6b70-76d5eebb7037

--- version.c
+++ version.c
@@ -1,1 +1,1 @@
-char *version_string = "bison++ Version 1.21-8, adapted from GNU bison by coetmeur@icdc.fr\n";
+char *version_string = "bison++ version 1.21-45, adapted from GNU Bison by coetmeur@icdc.fr\n";
